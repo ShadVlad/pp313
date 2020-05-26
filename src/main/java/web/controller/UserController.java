655535f5d06e60ed148a019e4238816b4d2c48c3
ShadVlad <wowchik.shadrin@yandex.ru>
2020-05-26 17:48:18 +0300

    Users table

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -2,11 +2,13 @@ package web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class UserController {
 
-    @GetMapping("/admin/users")
+    @GetMapping("/users")
     public String adminPage() {
         return "users";
     }
